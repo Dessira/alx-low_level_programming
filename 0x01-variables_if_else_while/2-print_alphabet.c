19d2d5aec7416deea8de231f229a7684b08d6621
Barine Desire <desirebarine16@gmail.com>
2022-03-11 22:57:56 +0100

    printing lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print alphabet in lower case using putchar.
+ * Return: 0
+ */
+int main(void)
+}
+	for (c = 'a'; c <= 'z'; c++)
+	{ 
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
