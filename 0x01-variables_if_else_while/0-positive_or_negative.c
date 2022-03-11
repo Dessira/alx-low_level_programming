cc023258d1c3aae9af8ab91b359f02254a14cbfb
Barine Desire <desirebarine16@gmail.com>
2022-03-11 22:14:52 +0100

    still playing with numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-* main - Entry point
+* main - main block 
 * description: Assign random number to n
 * print if n is positive, negative or zero.
 * Return: 0
@@ -14,12 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	printf("%i is positive\n", n);
 	else
-	if (n<0)
+	if (n < 0)
 	printf("%i is negative\n", n);
 	else
-	printf(%i is zero\n", n);
+	printf("%i is zero\n", n);
 	return (0);
 }
