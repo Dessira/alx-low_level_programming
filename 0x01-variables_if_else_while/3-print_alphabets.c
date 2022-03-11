6e3f72409bad18c11de89f75ec619ed52923855a
Barine Desire <desirebarine16@gmail.com>
2022-03-11 23:20:39 +0100

    printing lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print both lowercase and uppercase using putchar.
+ * Return: 0
+ */
+int main(0)
+{
+	char c; 
+	char m;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (m = 'A'; m <= 'Z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
