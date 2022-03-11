52796798ec76d00d14f99346d9a71914fb2e6430
Barine Desire <desirebarine16@gmail.com>
2022-03-11 23:57:48 +0100

    making exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all letters except the letter 'q' and 'e' using putchar.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
