249811fa7b62a260f0c76dd883aa8973cc471f70
Barine Desire <desirebarine16@gmail.com>
2022-03-15 20:53:25 +0100

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: Print _putchar followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = "_putchar"; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
