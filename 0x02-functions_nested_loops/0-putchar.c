6b26488c0b310d85b691bd13acc20f530ad2b304
Barine Desire <desirebarine16@gmail.com>
2022-03-15 21:23:33 +0100

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,12 @@
  */
 int main(void)
 {
-	char *c;
-	for (*c = "_putchar"; c++;)
+	char c[9] = "_putchar";
+	int i = 0
+
+	for (i = 0; i < 8; i++;)
 	{
-		_putchar(*c);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 	return (0);
