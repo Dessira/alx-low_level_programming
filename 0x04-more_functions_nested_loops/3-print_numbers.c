10a7529f50cc0f0477f3e077d5fbc026b6e5d007
Barine Desire <desirebarine16@gmail.com>
2022-03-17 22:02:06 +0100

    numbers speaking

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 /**
  *  print_numbers - prints numbers
  *  _putchar - prints characters
- *  Return: 0
+ *  Return: void 
  */
 
 void print_numbers(void)
 {
 	char c;
+
 	for (c = 0; c <= 9; c++)
 	{
 
 		_putchar(c);
 	}
+	_putchar('\n');
 }
