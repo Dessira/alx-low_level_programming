db2fade01ac81d43e7bad942049ef211bf1fccc2
Barine Desire <desirebarine16@gmail.com>
2022-03-17 21:56:00 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print_numbers - prints numbers
+ *  _putchar - prints characters
+ *  Return: 0
+ */
+
+void print_numbers(void)
+{
+	char c;
+	for (c = 0; c <= 9; c++)
+	{
+
+		_putchar(c);
+	}
+}
