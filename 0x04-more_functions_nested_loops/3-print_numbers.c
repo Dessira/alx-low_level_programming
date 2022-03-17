5363ea8252225de047670c7e5f8f83cf5d730bc6
Barine Desire <desirebarine16@gmail.com>
2022-03-18 00:34:30 +0100

    isdigit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,17 @@
 
 /**
  *  print_numbers - prints numbers.
- *  @c: integer character
  *  Return: void
  */
 
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = 0; c <= 9; c++)
+	for (i = 0; i <= 9; i++)
 	{
 
-		_putchar(c + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
