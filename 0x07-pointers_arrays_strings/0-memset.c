3a2ec42ca6120c382e3b240b8683b4eeffe5aa0e
Barine Desire <desirebarine16@gmail.com>
2022-03-28 15:12:26 +0100

    filling constants

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - function that fills in a constant b
+ * @s: pointer
+ * @b: constant
+ * @n: number of bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i > n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
