e2095a59c03c30ee4584bc31477cb801f6c83b27
Barine Desire <desirebarine16@gmail.com>
2022-03-28 21:42:40 +0100

    working with strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix string
+ * @s: string
+ * @accept: prefix
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k, bytes;
+
+	bytes = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		k = 0;
+		
+		for (j = 0; acccept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				bytes++;
+				k = 0;
+			}
+		}
+		if (k == 0)
+			return (bytes);
+	}
+	return (bytes);
+}
