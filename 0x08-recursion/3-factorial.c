9d3d39ee2b083f014e0bee8bf2a9dae3e8c5d437
Barine Desire <desirebarine16@gmail.com>
2022-03-30 22:11:24 +0100

    recursions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial
+ * @n: int
+ * Return: depends on input
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0);
+	return (0);
+	else
+		return (n * factorial(n - 1);
+}
