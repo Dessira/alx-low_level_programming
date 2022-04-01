dc32a3cdab48ee4409cf8a144f9eba9e9c4964da
Barine Desire <desirebarine16@gmail.com>
2022-04-01 21:19:49 +0100

    files for project argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (i < 1)
+		return (0);
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(agrv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
