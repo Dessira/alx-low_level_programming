7b73a13fe2b07967eebc448a2fd3af389fe4c05f
Barine Desire <desirebarine16@gmail.com>
2022-04-15 04:03:24 +0100

    fixing errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list input;
 	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-
 	va_start(input, n);
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(input, const unsigned int);
-	}
+
 	va_end((input);
 
 	return (sum);
