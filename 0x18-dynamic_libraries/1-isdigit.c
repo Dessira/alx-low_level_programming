54b44931bd8b0a4144cddedbb0ce43a838d58a36
Dessira <desirebarine16@gmail.com>
2022-06-13 15:56:36 +0000

    task 0

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - checks if character is a digit.
+ * @c: integer character
+ * Return: 0 or 1 depending on output.
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+
+	return (1);
+
+	return (0);
+
+}
---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: a string.
+ *Return: The integer value of the comverted string.
+ */
+
+int _atoi(char *s)
+{
+
+		return (0);
+
+}
